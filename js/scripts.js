4c0e6e8c781b8f95cb29b06b13bbf6380c062224
Jessica Engel & Brent Bailey <student@epicodus.com>
2015-06-02 09:36:24 -0700

    have address function and will print out full address with fullAddress prototype method

--- js/scripts.js
+++ js/scripts.js
@@ -8,6 +8,22 @@ function Contact(firstName, lastName){
 Contact.prototype.fullName = function() {
   return this.firstName + " " + this.lastName;
 };
+
+function Address(street, city, state) {
+  this.street = street;
+  this.city = city;
+  this.state = state;
+}
+
+Address.prototype.fullAddress = function(){
+  return this.street + ", " + this.city + ", " + this.state;
+};
+
+
+
+
+
+
 "use strict";
 
 
--- spec/specs.js
+++ spec/specs.js
@@ -5,12 +5,24 @@ describe('Contact', function(){
     expect(testContact.lastName).to.equal("Ellis");
     expect(testContact.addresses).to.eql([]);
   });
+
   it("adds the fullName method to all contacts", function() {
     var testContact = new Contact("Dave", "Matthews");
     expect(testContact.fullName()).to.equal("Dave Matthews");
   });
 
+});
 
+describe('Address', function(){
+  it("creates a new address with the given specs", function(){
+    var testAddress = new Address("4015 SE Hawthorne", "Portland", "OR");
+    expect(testAddress.street).to.equal("4015 SE Hawthorne");
+    expect(testAddress.city).to.equal("Portland");
+    expect(testAddress.state).to.equal("OR");
+  });
 
-
+  it("adds the fullAddress method to all addresses", function() {
+    var testAddress = new Address("123 Dad St.", "Dad City", "Dad State");
+    expect(testAddress.fullAddress()).to.equal("123 Dad St., Dad City, Dad State");
+  });
 });
